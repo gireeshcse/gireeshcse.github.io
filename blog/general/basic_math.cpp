24f8312346cdf5318352cd4f435e2ece4b30008b
Gireesh <gireeshcse@live.com>
2021-07-06 10:58:29 +0530

    programming update

--- blog/general/basic_math.cpp
+++ blog/general/basic_math.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cmath>
-#include <array>
+#include <cstring>
 
 using namespace std;
 
@@ -24,21 +24,104 @@ bool isPrime(int n){
 }
 
 bool* sieve(int n){
-    std::array<bool,sizeof(bool) * (n+1)> prime;
-    prime.fill(true)
+    // bool prime[n+1] is wrong since it returns local variable 
+    bool *prime = new bool[n+1];
+    // void* memset( void* str, int ch, size_t n);
+    // fill a byte string with a byte value
+    // memset works byte by byte
+    // for int it can set 0 or -1
+    memset(prime, true, sizeof(prime)*(n+1));
+    
+    prime[0] = false;
+    prime[1] = false;
+
+    int m = sqrt(n);
+    for(int p = 2; p <= m; p++){
+        if(prime[p]){
+            // if it is prime then eliminate the multiples of that number
+            // k start square of prime because less than it are already marked 
+            for(int k = p*p;k<= n;k+=p){
+                prime[k] = false;
+            }
+        }
+    }
 
     return prime;
 }
 
+int gcd(int a, int b){
+    int min = std::min(a,b);
+    int result = 1;
+    for(int i = min;i >= 2;i--){
+        if((a % i == 0) && (b % i == 0)){
+            return i;
+        }
+    }
+    return 1;
+}
+
+int gcd_euclid(int a, int b){
+    if(b == 0) return a;
+    return gcd_euclid(b, a % b);
+}
+
+int lcm(int a, int b){
+    return (a *b ) / gcd_euclid(a,b);
+}
+
 int main(){
 
-    int num;
-    cout << "Please enter a number:";
-    cin >> num;
-    if (isPrime(num) == true){
-        cout << "\n " << num << " is prime\n";
-    }else{
-        cout << "\n " << num << " is not prime\n";
+    int num,option,num1,num2;
+    cout << "\n1. Check a number is prime or not \n2. Prime numbers upto N(Sieve of Eratosthenes)" 
+        << "\n3.Normal GCD\n4.Euclid's GCD\n5. LCM\nPlease select the option:";
+    cin >> option;
+    switch (option)
+    {
+        case 1:{
+            cout << "\nPlease enter a number:";
+            cin >> num;
+            if(isPrime(num)){
+                cout << "\n" << num << " is a prime";
+            }else{
+                cout << "\n" << num << " is not a prime";
+            }
+            break;
+        }
+        case 2:{
+            cout << "\nPlease enter a number(N):";
+            cin >> num;
+            bool *primes = sieve(num);
+            for(int i=0;i<=num;i++){
+                if(primes[i]){
+                    cout << i << " ";
+                }
+            }
+            cout << "\n";
+            delete[] primes; //allocated memory must be deleted
+            break;
+        }
+        case 3:{
+            cout << "\nPlease enter a & b  numbers:";
+            cin >> num1 >> num2;
+            cout << gcd(num1,num2) << " is the GCD of " << num1 << " and " << num2 <<  "\n";
+            break;
+        }
+        case 4:{
+            cout << "\nPlease enter a & b  numbers:";
+            cin >> num1 >> num2;
+            cout << gcd_euclid(num1,num2) << " is the GCD of " << num1 << " and " << num2 <<  "\n";
+            break;
+        }
+        case 5:{
+            cout << "\nPlease enter a & b  numbers:";
+            cin >> num1 >> num2;
+            cout << lcm(num1,num2) << " is the LCM of " << num1 << " and " << num2 <<  "\n";
+            break;
+        }
+        default:
+            cout << "\n Invalid option";
+            break;
     }
+    return 0;
 }
 
--- blog/general/competitive_programming_part1.md
+++ blog/general/competitive_programming_part1.md
@@ -3,11 +3,7 @@
 #### Primes
 
 ```
-#include <iostream>
-#include <cmath>
-
-using namespace std;
-
+// #include <cmath>
 bool isPrime(int n){
 
     if(n <= 1) return false;
@@ -26,18 +22,6 @@ bool isPrime(int n){
 
     return true;
 }
-
-int main(){
-
-    int num;
-    cout << "Please enter a number:";
-    cin >> num;
-    if (isPrime(num) == true){
-        cout << "\n " << num << " is prime\n";
-    }else{
-        cout << "\n " << num << " is not prime\n";
-    }
-}
 ```
 
 ##### Sieve of Eratosthenes
@@ -45,3 +29,84 @@ int main(){
 To generate all prime from 2 to a given number n.
 
 It begins by assumming that all numbers are prime.It takes the first prime number and removes all of its multiples. It then applies the same method to next prime number.
+
+```
+bool* sieve(int n){
+    // bool prime[n+1] is wrong since it returns local variable 
+    bool *prime = new bool[n+1];
+    // void* memset( void* str, int ch, size_t n);
+    // fill a byte string with a byte value
+    // memset works byte by byte
+    // for int it can set 0 or -1
+    memset(prime, true, sizeof(prime)*(n+1));
+    
+    prime[0] = false;
+    prime[1] = false;
+
+    int m = sqrt(n);
+    for(int p = 2; p <= m; p++){
+        if(prime[p]){
+            // if it is prime then eliminate the multiples of that number
+            // k start square of prime because less than it are already marked 
+            for(int k = p*p;k<= n;k+=p){
+                prime[k] = false;
+            }
+        }
+    }
+
+    return prime;
+}
+```
+
+#### GCD(Greatest Common Divisor)
+
+ GCD of two numbers a and b is the greatest number that divides evenly into both a and b.
+
+ ```
+ int gcd(int a, int b){
+    int min = std::min(a,b);
+    int result = 1;
+    for(int i = min;i >= 2;i--){
+        if((a % i == 0) && (b % i == 0)){
+            return i;
+        }
+    }
+    return 1;
+}
+```
+
+ ##### Euclid’s algorithm
+
+* We begin by expressing the larger number in terms of the smaller number plus a remainder: 
+* wE iterate over the two numbers until a remainder of 0 is found.
+
+Example: GCD of 2336 and 1314
+2336 = 1314 x 1 + 1022
+We now do the same with 1314 and 1022:
+1314 = 1022 x 1 + 292
+We continue this process until we reach a remainder of 0:
+1022 = 292 x 3 + 146
+292 = 146 x 2 + 0
+
+```
+int gcd_euclid(int a, int b){
+    if(b == 0) return a;
+    return gcd_euclid(b, a % b);
+}
+```
+
+##### LCM(Least common Multiple) 
+
+* LCM of 6 and 9 is 18
+
+```
+int lcm(int a, int b){
+    return (a *b ) / gcd_euclid(a,b);
+}
+```
+
+
+
+####  Credits
+
+[Top Coder](https://www.topcoder.com/thrive/articles/Mathematics%20for%20Topcoders)
